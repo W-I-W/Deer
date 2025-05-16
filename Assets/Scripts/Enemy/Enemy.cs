87397b3b9c04be361a9ea074c6d4f9d513ea5652
W-I-W <StarBreak.pixel@gmail.com>
2025-05-16 22:00:19 +0500

    Bow

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,8 +4,11 @@ using DamageNumbersPro;
 using TMPro;
 
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Enemy : MonoBehaviour, IComponentHandler<Player>
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class Enemy : MonoBehaviour, IComponentHandler<Player>, ICharacterAnimation
 {
     [SerializeField] private Rigidbody2D m_Body;
     [SerializeField] private DamageNumber m_ViewDamagePrefab;
@@ -14,9 +17,20 @@ public class Enemy : MonoBehaviour, IComponentHandler<Player>
     [SerializeField] private int m_Health = 1;
     [SerializeField] private int m_ToTargetDistance = 2;
     [SerializeField] private float m_MoveSpeed = 2;
+    private Vector2 m_Move = Vector2.zero;
 
 
     private Player m_Player { get; set; }
+
+    public bool isIdle { get; set; } = false;
+
+    public float horizontal => m_Move.x;
+
+    public float vertical => m_Move.y;
+
+    public Vector2Int lastPress { get; set; } = Vector2Int.zero;
+
+    public float rotationTarget { get; set; } = 0;
 
 
     private void Start()
@@ -31,13 +45,18 @@ public class Enemy : MonoBehaviour, IComponentHandler<Player>
 
     public void OnFixedUpdate()
     {
+        rotationTarget = (m_Player.position - m_Body.position).normalized;
         if (Vector2.Distance(m_Body.position, m_Player.position) > m_ToTargetDistance) return;
         if (m_Player.state == RebuildStates.Deer)
         {
             m_Body.position += (m_Player.position - m_Body.position).normalized * m_MoveSpeed * Time.fixedDeltaTime;
+            Logic((m_Player.position - m_Body.position).normalized);
             return;
         }
         m_Body.position += (m_Body.position - m_Player.position).normalized * m_MoveSpeed * Time.fixedDeltaTime;
+        Logic((m_Body.position - m_Player.position).normalized);
+
+
     }
 
     public void TakeDamage(int damage)
@@ -53,6 +72,18 @@ public class Enemy : MonoBehaviour, IComponentHandler<Player>
     {
         m_Health += health;
         m_TextViewHealth.text = m_Health.ToString("00");
+    }
+
+    private void Logic(Vector2 value)
+    {
+        //Debug.Log(m_Body.linearVelocity);
+        m_Move = value; ;
+        isIdle = Mathf.Approximately(m_Move.x, 0) && Mathf.Approximately(m_Move.y, 0);
+        if (isIdle) return;
+        bool isHorZero = Mathf.Approximately(horizontal, 0);
+        int v = isHorZero ? (int)Mathf.Sign(vertical) : 0;
+        int h = isHorZero ? 0 : (int)Mathf.Sign(horizontal);
+        lastPress = new Vector2Int(h, v);
     }
 }
 
